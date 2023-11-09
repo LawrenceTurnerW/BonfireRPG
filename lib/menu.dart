ab05ffba9b62617d6bd2b9d39a41ba055295825d
LawrenceTurnerW <watawata.takatan@gmail.com>
2023-11-10 01:46:08 +0900

    スプラッシュ画面とメイン画面を作成

---
+++ lib/menu.dart
@@ -0,0 +1,86 @@
+import 'package:bonfire/bonfire.dart';
+import 'package:flame_splash_screen/flame_splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class Menu extends StatefulWidget {
+  const Menu({super.key});
+
+  @override
+  _MenuState createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  //スプラッシュスクリーンを既に見たかどうか
+  bool showSplash = false;
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 200),
+      child: showSplash ? buildMenu() : buildSplash(),
+    );
+  }
+
+  Widget buildMenu() {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const Text(
+                "BonfireRPG",
+                style: TextStyle(
+                    color: Colors.white, fontFamily: 'Normal', fontSize: 60.0),
+              ),
+              const SizedBox(height: 50),
+              SizedBox(
+                width: 150,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    minimumSize: const Size(100, 40),
+                  ),
+                  child: const Text(
+                    'Play',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Normal',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  onPressed: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => Game()),
+                    // );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildSplash() {
+    return FlameSplashScreen(
+      theme: FlameSplashTheme.dark,
+      onFinish: (BuildContext context) {
+        setState(() {
+          showSplash = true;
+        });
+      },
+    );
+  }
+}
